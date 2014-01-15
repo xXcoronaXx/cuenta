4e42e74ef7e62df5289dffc5bb1ae914ca8547e5
xXcoronaXx <acevedo.corona@gmail.com>
2014-01-15 18:19:32 +0100

    version 0.2
    Signed-off-by: xXcoronaXx <acevedo.corona@gmail.com>

--- js/general.js
+++ js/general.js
@@ -1,20 +1,45 @@
 var numero=0;
 $(document).ready(function() {
+	var incremento=1;
 	$("#numero").html(0);
+	$("#historial").html("");
+	//gestos
+	//txt = "jhjh008767n870";
+	//txt = txt.replace(/\D/g,'');
+	$$("#mas").swipeRight(function() {
+		var num=$("#mas").html();
+		num=num.replace(/\D/g,'');
+		console.log(num);
+		num=parseInt(num)+1;
+		incremento=num;
+    	$("#mas").html("+"+num);
+  	});
+	$$("#mas").swipeLeft(function() {
+    	var num=$("#mas").html();
+    	if(num!=1){
+		num=num.replace(/\D/g,'');
+		num=num-1;
+		incremento=num;
+    	$("#mas").html("+"+num);
+    	}
+  	});
 	$("#mas").click(function(){
-		numero=suma();
+		numero=suma(incremento);
 	});
 	$("#menos").click(function(){
-		numero=resta();
+		numero=resta(incremento);
 	});
 	$("#reset").click(function(){
 		numero=resetea();
 	});
+	$("#clear").click(function(){
+		clear();
+	});
 });
-function suma(){
+function suma(inc){
 	console.log("suma");
 	var nuevoNumero=0;
-	nuevoNumero=numero+1;
+	nuevoNumero=numero+inc;
 	$("#numero").html(nuevoNumero);
 
 	return nuevoNumero;
@@ -34,6 +59,22 @@ function resta(){
 
 function resetea(){
 	$("#numero").html(0);
-	//añadir al historial
+	$("#historial").prepend('<div><p class="fecha">'+fecha()+'</p>  <b>'+numero+'</b></div>');
 	return 0;
+}
+function clear(){
+	$("#numero").html(0);
+	$("#historial").html("");
+}
+function fecha(){//problema con la segunda ejecucion de fecha
+	var fecha = new Date();
+	mes = fecha.getMonth();
+	mes=mes+1;
+	diaMes = fecha.getDate();
+	diaSemana = fecha.getDay();
+	anio = fecha.getFullYear();
+	hora = fecha.getHours();
+	min = fecha.getMinutes();
+	seg = fecha.getSeconds();
+	return diaMes + "/" + mes + "/" + anio + " - " + hora + ":" + min + ":" + seg
 }
